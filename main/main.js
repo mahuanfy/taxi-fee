aa872dbca1c6d2bfaf513b65f5f8aca51fa8cf0e
mahuanfy <mahuanfy@foxmail.com>
2019-03-23 16:42:18 +0800

    Add answer

--- main/main.js
+++ main/main.js
@@ -1,4 +1,14 @@
-module.exports = function main(inputs) {
-    // write your code here...
-    return "price";
+module.exports = function taxiFee(input) {
+    let price = 0;
+
+    if (input.distance <= 2) {
+        price = 6;
+    } else if (2 < input.distance && input.distance <= 8) {
+        price = 6 + ((input.distance - 2) * 0.8);
+    } else {
+        price = 10.8 + ((input.distance - 8) * 1.2);
+    }
+    price += (input.parkTime * 0.25);
+
+    return Math.round(price);
 };
